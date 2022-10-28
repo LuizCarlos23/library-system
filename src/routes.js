cb8af773f54fcb4470d786f39fe88dcefe6a792e
Luiz Carlos <lc.estevao23@gmail.com>
2022-10-28 10:34:16 -0300

    Added exception middleware

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,11 @@ const express = require("express")
 
 const router = express.Router()
 
+const errorHandler = require("./middlewares/error-handler")
 const bookRegister = require("./controllers/book-register")
 
 router.post("/books/register", bookRegister)
+
+router.use(errorHandler)
 
 module.exports = router
