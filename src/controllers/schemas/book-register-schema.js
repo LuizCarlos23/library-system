190a4a35f8da5c151db56263a088b90469966f00
Luiz Carlos <lc.estevao23@gmail.com>
2022-10-27 21:53:24 -0300

    removed moment library

--- src/controllers/schemas/book-register-schema.js
+++ src/controllers/schemas/book-register-schema.js
@@ -1,8 +1,7 @@
 const Yup = require("yup")
-const Moment = require("moment")
 
 module.exports = Yup.object().shape({
     name: Yup.string().required(),
     author: Yup.string().required(),
-    release_date: Yup.date().max(new Date()).required()
+    release_date: Yup.date().required()
 })
