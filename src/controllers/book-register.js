44f23027f1f830dd6e19673f72968e91ee5fc4ca
Luiz Carlos <lc.estevao23@gmail.com>
2022-10-28 10:52:54 -0300

    Updated book registration controller

--- src/controllers/book-register.js
+++ src/controllers/book-register.js
@@ -1,11 +1,37 @@
 const Book = require("../models/books")
+const HttpCodeError = require("../exceptions/http-exception")
+
 const bookRegisterSchema =  require("./schemas/book-register-schema")
 
 
 async function bookRegister(req, res, next) {
-    console.log(req.body)
-    await bookRegisterSchema.validate(req.body).catch(err => console.log(err))
-    res.json({ message: "okay" })
+    const bookData = req.body
+
+    try {
+        await bookRegisterSchema.validate(bookData).catch(err => {
+            if(err.type == 'typeError'){
+                throw new HttpCodeError(400, err.params.label, err)
+            }
+            throw new HttpCodeError(400, err.message, err)
+        })
+
+        await Book.create({
+            name: bookData.name,
+            author: bookData.author,
+            release_date: bookData.release_date,
+        })
+        .then(result => {
+            console.log(result)
+        })
+        .catch(err => {
+            throw new HttpCodeError(500, "Internal error", err)
+        })
+
+
+        res.json({ message: "okay" })
+    } catch (error) {
+        next(error)
+    }
 }
 
 module.exports = bookRegister
