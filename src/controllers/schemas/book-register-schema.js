804d02505258e8d36eb38b217fa5ab9c4a68e425
Luiz Carlos <lc.estevao23@gmail.com>
2022-10-28 10:51:02 -0300

    Book register schema updated

--- src/controllers/schemas/book-register-schema.js
+++ src/controllers/schemas/book-register-schema.js
@@ -1,7 +1,7 @@
 const Yup = require("yup")
 
 module.exports = Yup.object().shape({
-    name: Yup.string().required(),
-    author: Yup.string().required(),
-    release_date: Yup.date().required()
+    name: Yup.string().required("Book name is required"),
+    author: Yup.string().required("Author name is required"),
+    release_date: Yup.date().label("Invalid date").max(new Date(), "Future dates are not accpeted").required("Book release date required")
 })
