a98ec1978422506805d35f3e9072d5f10b319e32
Luiz Carlos <lc.estevao23@gmail.com>
2022-10-29 15:24:33 -0300

    Added book search route

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,13 @@ const router = express.Router()
 
 const errorHandler = require("./middlewares/error-handler")
 const bookRegister = require("./controllers/book-register")
+const bookSearch = require("./controllers/book-search")
 
 router.post("/books/register", bookRegister.post)
 router.get("/books/register", bookRegister.get)
+
+router.post("/books/search", bookSearch.post)
+router.get("/books/search", bookSearch.get)
 
 router.use(errorHandler)
 
