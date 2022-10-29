ca0cb76eb47a833db44b0b1a057dc60378eecff6
Luiz Carlos <lc.estevao23@gmail.com>
2022-10-28 21:12:56 -0300

    Added views engibne and public files settings

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 require("dotenv").config()
 
+const path = require("path")
+
+const express = require("express")
+const mustacheExpress = require("mustache-express")
+
 const server = require("./src")
 
 const PORT = process.env.PORT || 3000
+
+//Enginer config
+server.set("views", path.resolve("views"))
+server.set("view engine", "mustache")
+server.engine("mustache", mustacheExpress())
+
+//Static files config
+server.use("/static", express.static("public"))
 
 server.listen(PORT ,() => {
     console.log("Server running on port " + PORT)
