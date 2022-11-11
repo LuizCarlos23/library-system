b48d5383beb2161ec19f3dae29c91bd0b179519b
Luiz Carlos <lc.estevao23@gmail.com>
2022-11-11 14:05:51 -0300

    Added route to delete book

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,17 @@ const router = express.Router()
 const errorHandler = require("./middlewares/error-handler")
 const bookRegister = require("./controllers/book-register")
 const bookSearch = require("./controllers/book-search")
+const bookDelete = require("./controllers/book-delete")
 
 router.post("/books/register", bookRegister.post)
 router.get("/books/register", bookRegister.get)
 
 router.post("/books/search", bookSearch.post)
 router.get("/books/search", bookSearch.get)
+
+router.post("/books/delete", bookDelete.post)
+
+router.get("", (req, res) => res.send("Error 404"))
 
 router.use(errorHandler)
 
